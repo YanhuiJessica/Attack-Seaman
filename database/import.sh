011a4812d02952a1dd13d8d68f160d8834fb4e3a
zhuyanxin <zhuyanxin@intern.douban.com>
2021-02-24 01:32:26 +0800

    fix:mongoimport 重复导入

--- database/import.sh
+++ database/import.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-mongo --host mongodb -u attackSeaman -p cuccs1sgreat --eval "db.mitre_attack.remove({});"
-mongoimport --host mongodb -u attackSeaman -p cuccs1sgreat --db attackSeaman --collection mitre_attack --type json --file /database/enterprise-attack.json --jsonArray
+
+mongoimport --host mongodb -u attackSeaman -p cuccs1sgreat --db attackSeaman --collection mitre_attack --drop --type json --file /database/enterprise-attack.json --jsonArray
 mongo --host mongodb -u attackSeaman -p cuccs1sgreat --eval "db.mitre_attack.find().forEach(function(doc){doc.created = new Date(doc.created);db.mitre_attack.save(doc)});"
 mongo --host mongodb -u attackSeaman -p cuccs1sgreat --eval "db.mitre_attack.find().forEach(function(doc){doc.modified = new Date(doc.modified);db.mitre_attack.save(doc)});"
