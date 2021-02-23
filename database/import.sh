bf95c6522041c9cfc96bb4f50fdc3d966c567d07
zhuyanxin <zhuyanxin@intern.douban.com>
2021-02-23 13:25:50 +0800

    update Dockerfile config

---
+++ database/import.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mongoimport --host mongodb -u attackSeaman -p cuccs1sgreat --db attackSeaman --collection mitre_attack --type json --file /database/enterprise-attack.json --jsonArray
+mongo --host mongodb -u attackSeaman -p cuccs1sgreat --eval "db.mitre_attack.find().forEach(function(doc){doc.created = new Date(doc.created);db.mitre_attack.save(doc)});"
+mongo --host mongodb -u attackSeaman -p cuccs1sgreat --eval "db.mitre_attack.find().forEach(function(doc){doc.modified = new Date(doc.modified);db.mitre_attack.save(doc)});"
