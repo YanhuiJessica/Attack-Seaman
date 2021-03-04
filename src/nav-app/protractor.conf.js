e4c2d5c437ad47e8afb30e23facb733c70476528
YanhuiJessica <137126578@qq.com>
2021-03-04 14:02:17 +0800

    修改为以域名方式访问nav

--- src/nav-app/protractor.conf.js
+++ src/nav-app/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://attack-seaman.com:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
